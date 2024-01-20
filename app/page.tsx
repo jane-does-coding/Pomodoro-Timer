465565fb6bb895c1b5c237fda456fac8112e88bc
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-19 17:19:42 -0700

    initial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Categories</h1>
+      <h1>Posts</h1>
+      <h1>Load More</h1>
+    </section>
+  );
+};
+
+export default Home;
---
+++ components/AuthProviders.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AuthProviders = () => {
+  return <div>AuthProviders</div>;
+};
+
+export default AuthProviders;
